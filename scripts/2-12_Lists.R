6b736c36c30f2b0f26550531b3623dabb875b664
belinskyc <36079242+belinskyc@users.noreply.github.com>
2024-02-08 11:21:00 -0500

    beat you!

--- scripts/2-12_Lists.R
+++ scripts/2-12_Lists.R
@@ -1,4 +1,4 @@
-{
+{   
   rm(list=ls());                         # clear the Environment tab
   options(show.error.locations = TRUE);  # show line numbers on error
   library(package=ggplot2);              # include all GGPlot2 functions
